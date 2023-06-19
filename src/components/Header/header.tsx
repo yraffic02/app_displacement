1b10727ad0d44bf16c8664e55b62cf7d0b06efae
yraffic <yraffic@gmail.com>
2023-06-19 10:43:42 -0300

    feat: header styling done

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,11 +4,12 @@ import NavigationIcon from '@mui/icons-material/Navigation';
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center absolute top-0 z-50 w-screen p-4">
-      <Button variant="contained">Contained</Button>
-      <Fab variant="extended">
-        <NavigationIcon />
-      </Fab>
+    <header className="flex justify-between items-center absolute top-0 z-50 w-screen p-4 bg-slate-200">
+      <h1>Desloque-se <NavigationIcon /></h1>
+      <div className="flex gap-2 ">
+        <Button color="primary" variant="text">Fazer login</Button>
+        <Button variant="outlined">Cadastre-se</Button>
+      </div>
     </header>
   );
 }
