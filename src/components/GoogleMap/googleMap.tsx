ccd90f50419d66a887708110152772d82816e3f3
yraffic <yraffic@gmail.com>
2023-06-19 23:24:08 -0300

    fix: corrected fix, rename variable next_public

--- src/components/GoogleMap/googleMap.tsx
+++ src/components/GoogleMap/googleMap.tsx
@@ -1,16 +1,22 @@
 'use client'
 import { Button, Skeleton, TextField } from "@mui/material";
 import NavigationIcon from '@mui/icons-material/Navigation';
-import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import {
+    GoogleMap,
+    Marker,
+    useJsApiLoader,
+    Autocomplete
+} from "@react-google-maps/api";
 import { useState } from "react";
 
 const center = { lat: 48.8584, lng: 2.2945 }
 
 export default function GoogleMaps() {
-    const apiKey = process.env.API_KEY_MAPS || ''
     const [map, setMap] = useState<google.maps.Map | any>(null)
-    const { isLoaded } = useJsApiLoader({googleMapsApiKey: apiKey})
-
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_API_KEY_MAPS || '',
+        libraries: ['places']
+    })
 
     if (!isLoaded) {
         return <Skeleton
@@ -24,14 +30,20 @@ export default function GoogleMaps() {
         <>
             <div className="flex flex-col gap-2 p-2">
                 <div className="flex gap-4">
-                    <TextField id="standard-basic" label="origem" variant="standard" />
-                    <TextField id="standard-basic" label="destino" variant="standard" />
+                    <Autocomplete>
+                        <TextField id="standard-basic" label="origem" variant="standard" />
+                    </Autocomplete>
+
+                    <Autocomplete>
+                        <TextField id="standard-basic" label="destino" variant="standard" />
+                    </Autocomplete>
+
                     <Button variant="outlined">calcular</Button>
                 </div>
                 <div className="flex gap-4" >
                     <p>distancia</p><span></span>
                     <p>duração</p><span></span>
-                    <div onClick={()=> map.panTo(center)}>
+                    <div onClick={() => map.panTo(center)}>
                         <NavigationIcon />
                     </div>
                 </div>
@@ -47,7 +59,7 @@ export default function GoogleMaps() {
                     fullscreenControl: false,
                     zoomControl: false
                 }}
-                onLoad={(map)=> setMap(map)}
+                onLoad={(map) => setMap(map)}
             >
                 <Marker position={center} />
             </GoogleMap>
